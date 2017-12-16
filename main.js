8ad7ccb4ef0f7cb441779aecfa7cc109b35d16c7
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2017-12-16 12:03:19 -0500

    initial check in

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+const url = require('url');
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: '#ffffff'
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+      }));
+
+    win.on('closed', function(){
+        win = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+
+app.on('activate', function(){
+    if(win === null){
+        createWindow();
+    }
+})
