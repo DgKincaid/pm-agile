09e409fdf927ca4dbf59395a98c935ea88ad620e
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2018-01-12 15:46:35 -0500

    updated task state

--- main-process/tasks.js
+++ main-process/tasks.js
@@ -5,23 +5,30 @@ var tasks = [
       id: 2,
       name: 'Test Task 2',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis porttitor laoreet. Nam molestie nunc sed velit tincidunt, ac cursus velit lacinia. Duis semper urna quis gravida condimentum. ",
-      state: 'To Do',
+      state: 'to_do',
       assigned_to: 'John Smith'
     },
     {
       id: 3,
       name: 'Test Task 1',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis porttitor laoreet. Nam molestie nunc sed velit tincidunt, ac cursus velit lacinia. Duis semper urna quis gravida condimentum. ",
-      state: 'To Do',
+      state: 'to_do',
       assigned_to: 'John Smith'
     },
     {
       id: 4,
       name: 'Test Task 3',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis porttitor laoreet. Nam molestie nunc sed velit tincidunt, ac cursus velit lacinia. Duis semper urna quis gravida condimentum. ",
-      state: 'To Do',
+      state: 'to_do',
       assigned_to: 'John Smith'
-    }
+    },
+    {
+        id: 5,
+        name: 'Test Task 3',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis porttitor laoreet. Nam molestie nunc sed velit tincidunt, ac cursus velit lacinia. Duis semper urna quis gravida condimentum. ",
+        state: 'in_progress',
+        assigned_to: 'John Smith'
+      }
   ]
 
 ipc.on('add-task', function(event, arg){
