e7d9f4cc61fca861666bedaa1ba1e4c5fd751183
Douglas Kincaid <dgkincaid16@gmail.com>
2018-11-17 19:17:16 -0500

    starting over

--- client/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './side-nav/side-nav.component';
-export * from './task-board/task-board.component';
-export * from './top-bar/top-bar.component';
-export * from './task-column/task-column.component';
-export * from './task/task.component';
-export * from './add/add.component';
